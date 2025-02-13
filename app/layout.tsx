c880405b05234a0814365122c7edf3eb585ce035
Sourov Biswas <itsourov12@gmail.com>
2025-02-13 19:52:10 +0600

    refactor: remove unused KaTeX stylesheet link from layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,14 +22,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en" suppressHydrationWarning>
-            <head>
-                <link
-                    rel="stylesheet"
-                    href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css"
-                    integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC"
-                    crossOrigin="anonymous"
-                />
-            </head>
+
             <body className={inter.className}>
                 <ThemeProvider
                     attribute="class"
