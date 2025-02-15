07dd4ca6f6d8a17c35ba21d9e1589dfde0bbada9
Sourov Biswas <itsourov12@gmail.com>
2025-02-15 23:39:04 +0600

    feat: remove outdated competitive programming guide and add a new example post

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {images: {
-        domains: ['lh3.googleusercontent.com', 'sgp1.digitaloceanspaces.com', 'cdn.diuacm.com'],
+const nextConfig: NextConfig = {
+    images: {
+        domains: ['lh3.googleusercontent.com', 'nextacm.sgp1.cdn.digitaloceanspaces.com', 'cdn.diuacm.com'],
     },
 };
 
