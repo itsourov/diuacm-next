b1bd8112f862ef47465f4643a23c8db18ffd1edc
Sourov Biswas <itsourov12@gmail.com>
2025-02-20 04:23:08 +0600

    chore: remove standalone output configuration from Next.js settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: 'standalone',
     images: {
         domains: ['lh3.googleusercontent.com', 'nextacm.sgp1.cdn.digitaloceanspaces.com', 'cdn.diuacm.com'],
     },
