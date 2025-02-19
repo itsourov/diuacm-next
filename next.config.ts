9e8eeeb43cfe6f49fbde980ddd0b442623e73447
Sourov Biswas <itsourov12@gmail.com>
2025-02-20 04:12:26 +0600

    feat: enable standalone output in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output: 'standalone',
     images: {
         domains: ['lh3.googleusercontent.com', 'nextacm.sgp1.cdn.digitaloceanspaces.com', 'cdn.diuacm.com'],
     },
